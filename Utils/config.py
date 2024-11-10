7f0d58b12282631f00774cbfa1981557df8128e2
Sambath82 <kinsambath82@gmail.com>
2024-11-10 00:35:51 -0500

    Add files via upload
    add two py files

---
+++ Utils/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Define the main project path
+RootPath = '/Users/sam/Desktop/GitHub/Project/Home_Credit_Default_Risk/'
+
+# Define paths to specific directories
+RawDataPath = f'{RootPath}Data/RawData/'
+CleanDataPath = f'{RootPath}Data/CleanData/'
+AssetPath = f'{RootPath}Assets/'
+
+
